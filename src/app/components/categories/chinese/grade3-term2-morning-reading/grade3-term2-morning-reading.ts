cad548ca5258f818a6b47f400faff43464285d65
i340818 <will.sun@sap.com>
2026-03-05 11:48:57 +0800

    Deploy 2026-03-05 11-48-57

--- src/app/components/categories/chinese/grade3-term2-morning-reading/grade3-term2-morning-reading.ts
+++ src/app/components/categories/chinese/grade3-term2-morning-reading/grade3-term2-morning-reading.ts
@@ -1,23 +1,20 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { SafePipe } from '../../../../pipes/safe.pipe';
 
 @Component({
   selector: 'app-grade3-term2-morning-reading',
   standalone: true,
-  imports: [CommonModule, NgxExtendedPdfViewerModule, SafePipe],
+  imports: [CommonModule, SafePipe],
   templateUrl: './grade3-term2-morning-reading.html',
   styleUrl: './grade3-term2-morning-reading.scss',
 })
 export class Grade3Term2MorningReadingComponent {
   pdfPath: string;
-  isMobile: boolean;
 
   constructor(private router: Router) {
     this.pdfPath = '/assets/resources/categories/chinese/grade3-term2-characters/三下语文寒假预习每课晨读单.pdf';
-    this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || window.innerWidth <= 768;
   }
 
   goBack() {
